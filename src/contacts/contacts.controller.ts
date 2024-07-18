ca079c98693e947f988b26814dcc139844b511a5
utkir <macbookpro@MacBook-Pro-MacBook.local>
2024-07-18 17:26:38 +0500

    chore: contact is added

--- src/contacts/contacts.controller.ts
+++ src/contacts/contacts.controller.ts
@@ -2,12 +2,14 @@ import { Controller, Get, Post, Body } from '@nestjs/common';
 import { ContactsService } from './contacts.service';
 import { CreateContactDto } from './dto/create-contact.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { Public } from 'src/auth/decorators/is-public.decorator';
 
 @ApiTags('Contact us ')
 @Controller('contacts')
 export class ContactsController {
     constructor(private readonly contactsService: ContactsService) {}
 
+    @Public()
     @Post()
     create(@Body() createContactDto: CreateContactDto) {
         return this.contactsService.create(createContactDto);
